b683eab44271d943afa4e6f72f7224172f1273a3
eory96 <21500057@handong.edu>
2018-04-06 20:06:44 +0900

    add javadoc commit

--- src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesAssociate.java
@@ -1,28 +1,50 @@
-package edu.handong.csee.java.lab09;
-
-public class SalesAssociate {
+package edu.handong.csee.java.lab09;//package name
+/**
+ * this class make getter and setter 
+ * it make SlaesRepoter class can use SalesAssociate array 
+ * @author samsung
+ *
+ */
+public class SalesAssociate {//class name is SalesAssociate
 	
-	private String name;
-	private double sales;
+	private String name;//this is for receiving name
+	private double sales;//this is for receiving sales
 	
-	public void SalesAssociate() {
-		this.name = null;
-		this.sales = 0;
+	/**
+	 * this method is set name ane sales
+	 */
+	public void SalesAssociate() {//this method no has return value
+		this.name = null;//this class name is initialization to null
+		this.sales = 0;//this class variable sales initialization to 0
 	}
 	
-	public void setName(String name) {
-		this.name = name;
+	/**
+	 * this method is get name from SalesReporter class
+	 */
+	public void setName(String name) {//this method have parameter and type is String
+		this.name = name;//this class variable name initialize to name that from SalesReporter class
 	}
 	
-	public void setSales(double sales) {
-		this.sales = sales;
+	/**
+	 * this method is get sales performance from SalesReporter class
+	 * @param sales
+	 */
+	public void setSales(double sales) {//this method have parameter and type is double
+		this.sales = sales;//this class variable sales initialize to sales that from SalesReporter class
 	}
 	
-	public String getName() {
-		return this.name;
+	/**
+	 * this method is return name to SalesReporter
+	 * @return
+	 */
+	public String getName() {//this method doensn't have parameter. use for calling name
+		return this.name;//return value is name that declare in this class
 	}
-	
-	public double getSales() {
-		return this.sales;
+	/**
+	 * this method is return sales to SalesReporter
+	 * @return
+	 */
+	public double getSales() {//this method use for calling sales that store name of other class variable
+		return this.sales;//return value is sales that declare in this class
 	}
 }
