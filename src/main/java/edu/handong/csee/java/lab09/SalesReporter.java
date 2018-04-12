95941bdba407cb36458cde03c15426f2d7e5bd35
eory96 <21500057@handong.edu>
2018-04-13 00:37:52 +0900

    array list commit

--- src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
@@ -1,5 +1,7 @@
 package edu.handong.csee.java.lab09;//package name
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Scanner;//use for keyboard input
 /**
  * This project show Sales report. Input a number of people and report there sales result
@@ -9,9 +11,8 @@ import java.util.Scanner;//use for keyboard input
 public class SalesReporter {
 	private double highestSales=0;//use for select highest sales result
 	private double averageSales=0;//use for calculating average
-	private SalesAssociate[] team;//declare array
-	private int topNum ;//use for classify highest result performance
-	
+	ArrayList<SalesAssociate> team = new ArrayList<SalesAssociate>();;
+	int topNum=0;
 	/**
 	 * main method call other method and show result
 	 * SalesReporter is instantiation to callData and call there method
@@ -29,22 +30,32 @@ public class SalesReporter {
 	 * input number of salesmen and input there data as using array
 	 */
 	public void getData() {//method name is getData
-		System.out.println("Enter number of sales associates : ");//printout Enter number of sales associates : 
-		Scanner keyboard = new Scanner(System.in);//this is for using keyboard
-		int numberOfAssociates = keyboard.nextInt();//declare numberOfAssociate because input number of salesmen
-		team = new SalesAssociate[numberOfAssociates];//declaration team array
 		
-		for(int i=0;i<numberOfAssociates;i++) {//loop for input data about salesmen
-			System.out.println("Enter data for associate number "+(i+1));//printout Enter data for associate number  
-			System.out.println("Enter name of sales associate : ");//printout Enter name of sales associate :  
-			keyboard.nextLine();//when enter the numberOfAssociate enter is not delete. so, this command delete enter from memory
-			String name=keyboard.nextLine();//input name of salesman
+		Scanner keyboard = new Scanner(System.in);//this is for using keyboard
+		//team = new ArrayList<SalesAssociate>();
+		
+		
+		boolean done = false;
+		int a=0;
+		
+		while(!done) {
+			SalesAssociate result = new SalesAssociate();
+			System.out.println("Enter data for associate number"+(a+1));
+			a++;
+			System.out.println("Enter name of sales associate : ");
+			String name = keyboard.nextLine();
 			
-			System.out.println("Enter associate's sales : ");//print out Enter associate's sales : 
-			double sales=keyboard.nextDouble();//input sales of salesman
-			team[i]= new SalesAssociate();//instantiation  team array
-			team[i].setName(name);//save name to team array
-			team[i].setSales(sales);//save sales to team array
+			System.out.println("Enter associate's sales : ");
+			double sales = keyboard.nextDouble();
+			result.setName(name);
+			result.setSales(sales);
+			team.add(result);
+			keyboard.nextLine();
+			
+			System.out.println("Do you want to add another salesman?");
+			String ans = keyboard.nextLine();
+			if(!ans.equalsIgnoreCase("yes"))
+				done = true;
 		}
 	}
 	
@@ -52,41 +63,41 @@ public class SalesReporter {
 	 * this method calculate average sales and highest performance salesman
 	 */
 	public void computeStats() {//this is computeSatas method
-		int sum=0;//use for calculating average
-		for(int i=0;i<team.length;i++) {//loop for sum all sales (team.length = number of salesman)
-			sum+=team[i].getSales();//sum all sales performance
+		double sum=0;//use for calculating average
+		for(int i=0;i<team.size();i++) {//loop for sum all sales (team.length = number of salesman)
+			sum=sum + team.get(i).getSales();//sum all sales performance
 		}
-		averageSales = sum/team.length;//calculate average
+		averageSales = sum/team.size();//calculate average
 		System.out.println("Average sales per associate is " + averageSales);//printout Average sales per associate is and average
 		
-		for(int a=0;a<team.length;a++) {//loop for choosing highest performer
-			if(highestSales<team[a].getSales()) {//Compared with a larger number.
-				highestSales = team[a].getSales();// if highestSales is less than change highestSales comparison target
+		for(int a=0;a<team.size();a++) {//loop for choosing highest performer
+			if(highestSales < team.get(a).getSales()) {//Compared with a larger number.
+				highestSales = team.get(a).getSales();// if highestSales is less than change highestSales comparison target
 				topNum =a;//this is for Distinguishing highest number in array
 			}
 		}
 		System.out.println("The highest sales figure is " + highestSales);//print out "The highest sales figure is (highestSales)" 
 	}
-	
+
 	/**
 	 * this method display another salesmen performance
 	 */
 	public void displayResults() {//this is displayResults method
 		System.out.println("The following had the highest sales : ");//printout "The following had the highest sales : "
-		System.out.println("Name : " + team[topNum].getName());//printout "Name : (the highest performer)"
-		System.out.println("Sales : " + team[topNum].getSales());//printout "Sales : (the highest performer's sales)"
-		System.out.println("$"+(team[topNum].getSales()-averageSales) +" above the average.");//printout "$ (difference of average and sales) above the average."
+		System.out.println("Name : " + (team.get(topNum)).getName());//printout "Name : (the highest performer)"
+		System.out.println("Sales : " + (team.get(topNum)).getSales());//printout "Sales : (the highest performer's sales)"
+		System.out.println("$"+((team.get(topNum)).getSales()-averageSales) +" above the average.");//printout "$ (difference of average and sales) above the average."
 		System.out.print("\n");//printout blank line
 		System.out.println("The rest performed as follows :");//printout "The rest performed as follows :"
 		
-		for(int i=0;i<team.length;i++) {//loop for printout another salesman
+		for(int i=0;i<team.size();i++) {//loop for printout another salesman
 			if(i==topNum) continue;//don't printout highest performer data
 			
-			System.out.println("Name : "+team[i].getName());//printout "Name : (salesman that is not highest performer)"
-			System.out.println("Sales : "+team[i].getSales());//printout "Sales : (sales that is not highest performer)"
-			if(averageSales>=team[i].getSales())//if averageSales is larger than team[i].getSales() action below sentence
-				System.out.println("$" + (averageSales-team[i].getSales())+"below the average");//printout "$ (difference of average and sales) below the average"
-			else System.out.println("$" + (team[i].getSales()-averageSales)+"above the average");//if averageSales is less than team[i].getSales() printout "$ (difference of average and sales) above the average"
+			System.out.println("Name : "+(team.get(i)).getName());//printout "Name : (salesman that is not highest performer)"
+			System.out.println("Sales : "+(team.get(i)).getSales());//printout "Sales : (sales that is not highest performer)"
+			if(averageSales>=(team.get(i)).getSales())//if averageSales is larger than team[i].getSales() action below sentence
+				System.out.println("$" + (averageSales-(team.get(i)).getSales())+"below the average");//printout "$ (difference of average and sales) below the average"
+			else System.out.println("$" + ((team.get(i)).getSales()-averageSales)+"above the average");//if averageSales is less than team[i].getSales() printout "$ (difference of average and sales) above the average"
 		}
 	}
 }
